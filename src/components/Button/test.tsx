71921cfaff378cfde56228d649ad9e5a1dcc25ed
Leandro Guina <guina86@gmail.com>
2022-07-08 10:02:46 -0300

    feat: add Button component

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,56 @@
+import { AddShoppingCart } from '@styled-icons/material-outlined'
+import { RenderResult, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Button, { ButtonProps } from '.'
+
+describe('<Button />', () => {
+  const renderSut = (args?: Omit<ButtonProps, 'children'>): RenderResult =>
+    renderWithTheme(<Button {...args}>Buy now</Button>)
+
+  it('should render medium size by default', () => {
+    const { container } = renderSut()
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render small size when passed ', () => {
+    renderSut({ size: 'small' })
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '3rem',
+      padding: '0.8rem 1rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render large size when passed ', () => {
+    renderSut({ size: 'large' })
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    })
+  })
+
+  it('should render with full width when passed ', () => {
+    renderSut({ fullWidth: true })
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
+
+  it('should render an icon when passed', () => {
+    renderSut({ icon: <AddShoppingCart data-testid="icon" /> })
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+})
