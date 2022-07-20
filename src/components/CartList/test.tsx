e97c2427b8ee2a9919f51c16f29664727f2ee08c
Leandro Guina <guina86@gmail.com>
2022-07-20 13:29:11 -0300

    feat: add hasButton props to CartList component

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -1,11 +1,11 @@
 import { RenderResult, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-import CartList from '.'
+import CartList, { CartListProps } from '.'
 import itemsMock from './mock'
 
 describe('<CartList />', () => {
-  const renderSut = (): RenderResult =>
-    renderWithTheme(<CartList items={itemsMock} total="R$ 430,00" />)
+  const renderSut = (props?: Partial<CartListProps>): RenderResult =>
+    renderWithTheme(<CartList items={itemsMock} total="R$ 430,00" {...props} />)
 
   it('should render the heading', () => {
     const { container } = renderSut()
@@ -14,5 +14,11 @@ describe('<CartList />', () => {
     expect(screen.getByText('R$ 430,00')).toHaveStyle({ color: '#F231A5' })
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('should render the button', () => {
+    renderSut({ hasButton: true })
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
   })
 })
